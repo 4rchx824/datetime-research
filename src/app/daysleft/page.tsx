45902b688e6861c7c9421a3dcb49af00f75e6980
Jocelyn <115977531+tokziqijocelyn@users.noreply.github.com>
2023-12-31 03:31:54 +0800

    I think maybe this might be it but we shall see

--- src/app/daysleft/page.tsx
+++ src/app/daysleft/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { api } from "@/trpc/react";
 import { DateTime, Interval } from "luxon";
 
@@ -18,25 +18,71 @@ const Page: React.FC<PageProps> = () => {
   const tzid = Intl.DateTimeFormat().resolvedOptions().timeZone;
   console.log(tzid);
 
+  type date = {
+    id: number;
+    name: string;
+    createdAt: Date;
+    updatedAt: Date;
+    deadline: Date;
+  };
+
+  const YourComponent = () => {
+
+    const [position, setPosition] = useState(null);
+    
+    useEffect(() => {
+      const fetchDates = async () => {
+        try {
+          var position = await new Promise((resolve, reject) => {
+            navigator.geolocation.getCurrentPosition(resolve, reject);
+          });
+          
+          console.log("THIS IS THE POSITION", position);
+          
+          // Now you can use position in your logic
+          // ...
+  
+        } catch (error) {
+          console.error("Error getting geolocation:", error);
+        }
+      };
+  
+      fetchDates();
+    }, []); // Run this effect only once when the component mounts
+  
+    return (
+      <h1>{position}</h1>
+    );
+  };
+
   return (
     <div>
       <h1>Days left for each deadline</h1>
       <h1>Time Zone through system: {tzid}</h1>
-      <h1>Time Zone through ip geolocation: NTO YET DONE</h1>
+      <h1>Time Zone through ip geolocation: <YourComponent/></h1>
       <div>
         {!deadlineData && <div>No</div>}
-        {deadlineData?.map((date: any) => {
-          const deadline = DateTime.fromISO(date.deadline.toISOString());
+        {deadlineData?.map((date: date) => {
+          // so that I can use Luxon's date functions
+          let deadline = DateTime.fromISO(date.deadline.toISOString());
+          
           const now = DateTime.local();
           const i = Interval.fromDateTimes(now, deadline);
-          // const test = Interval.fromDateTimes(now, );
+          const test = Interval.fromDateTimes(now, date.deadline);
 
           return (
             <div key={date.id}>
+              =============================
               <h1>{date.name}</h1>
-              <h1>Days from now: {i.length("days")}</h1>
-              <h1>TODAY === {now.toString()}</h1>
-              <h1>Deadline: {deadline.toString()}</h1>
+              <h1>Days from now: {Math.round(i.length("days"))} days</h1>
+              <h1>Days from now: {Math.round(test.length("days"))} days</h1>
+              <h1>TODAY === {now.toString()} in local time</h1>
+              <h1></h1>
+              <h1>Deadline: {deadline.toLocaleString(DateTime.DATETIME_FULL)} in local time</h1>
+              <h1>
+                Original Deadline in database {date.deadline.toISOString()} in UTC
+                
+              </h1>
             </div>
           );
         })}
