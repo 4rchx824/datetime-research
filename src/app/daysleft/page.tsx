42b9d677ec5006c5493c99f8216358314afc4c98
Jocelyn <115977531+tokziqijocelyn@users.noreply.github.com>
2023-12-31 00:39:59 +0800

    installed luxon and dayjs

--- src/app/daysleft/page.tsx
+++ src/app/daysleft/page.tsx
@@ -1,34 +1,31 @@
 "use client";
 import React from "react";
 import { api } from "@/trpc/react";
-import { format, parseISO } from "date-fns";
 
 interface PageProps {}
 
 const Page: React.FC<PageProps> = () => {
-  const { data: deadlineData, error } = api.date.getAllDates.useQuery();
+  const { data: deadlineData, isError, isLoading } = api.date.getAllDates.useQuery();
 
   console.log("DEADLINE:", deadlineData);
+  const now = new Date();
+  //getting the timezone
+  const tzid = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  console.log(tzid);
+  
 
   return (
     <div>
       <h1>Days left for each deadline</h1>
+      <h1>Time Zone through system: {tzid}</h1>
+      <h1>Time Zone through ip geolocation: ???</h1>
       <div>
         {deadlineData ? (
           <div>
-            <p>
-              Your most recent post:{" "}
-              {format(
-                parseISO(deadlineData[0]?.deadline?.toISOString()!),
-                "MM/dd/yyyy",
-              )}
-            </p>
-            <p>
-              Time diff: {deadlineData[0]?.deadline?.toLocaleTimeString()}
-            </p>
-            <p>
-              Today date: {new Date().toString()}
-            </p>
+            <p>Your most recent post: </p>
+            <p>Time diff: {deadlineData[0]?.deadline?.toLocaleTimeString()}</p>
+            <p>WYA {tzid}</p>
+
           </div>
         ) : (
           <p>You have no posts yet.</p>
