2da7d101832b13d5114dbf034cd3000769be5559
Jocelyn <115977531+tokziqijocelyn@users.noreply.github.com>
2023-12-31 01:42:03 +0800

    Going to fucking kms

--- src/app/daysleft/page.tsx
+++ src/app/daysleft/page.tsx
@@ -1,35 +1,40 @@
 "use client";
 import React from "react";
 import { api } from "@/trpc/react";
+import { DateTime } from "luxon";
 
 interface PageProps {}
 
 const Page: React.FC<PageProps> = () => {
-  const { data: deadlineData, isError, isLoading } = api.date.getAllDates.useQuery();
+  const {
+    data: deadlineData,
+    isError,
+    isLoading,
+  } = api.date.getAllDates.useQuery();
 
   console.log("DEADLINE:", deadlineData);
   const now = new Date();
   //getting the timezone
   const tzid = Intl.DateTimeFormat().resolvedOptions().timeZone;
   console.log(tzid);
-  
 
   return (
     <div>
       <h1>Days left for each deadline</h1>
       <h1>Time Zone through system: {tzid}</h1>
-      <h1>Time Zone through ip geolocation: ???</h1>
+      <h1>Time Zone through ip geolocation: NTO YET DONE</h1>
       <div>
-        {deadlineData ? (
-          <div>
-            <p>Your most recent post: </p>
-            <p>Time diff: {deadlineData[0]?.deadline?.toLocaleTimeString()}</p>
-            <p>WYA {tzid}</p>
+        {!deadlineData && <div>No</div>}
+        {deadlineData?.map((date) => {
+          const deadline = DateTime.fromISO(date.deadline.toDateString());
 
-          </div>
-        ) : (
-          <p>You have no posts yet.</p>
-        )}
+          return (
+            <div key={date.id}>
+              <h1>{date.name}</h1>
+              <h1>Days from now: {deadline.toString()}</h1>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
