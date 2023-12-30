8bd9406c39787e648f2d4f5afce4b4025eccc479
Jocelyn <115977531+tokziqijocelyn@users.noreply.github.com>
2023-12-31 02:04:53 +0800

    help

--- src/app/daysleft/page.tsx
+++ src/app/daysleft/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import { api } from "@/trpc/react";
-import { DateTime } from "luxon";
+import { DateTime, Interval } from "luxon";
 
 interface PageProps {}
 
@@ -25,13 +25,18 @@ const Page: React.FC<PageProps> = () => {
       <h1>Time Zone through ip geolocation: NTO YET DONE</h1>
       <div>
         {!deadlineData && <div>No</div>}
-        {deadlineData?.map((date) => {
-          const deadline = DateTime.fromISO(date.deadline.toDateString());
+        {deadlineData?.map((date: any) => {
+          const deadline = DateTime.fromISO(date.deadline.toISOString());
+          const now = DateTime.local();
+          const i = Interval.fromDateTimes(now, deadline);
+          // const test = Interval.fromDateTimes(now, );
 
           return (
             <div key={date.id}>
               <h1>{date.name}</h1>
-              <h1>Days from now: {deadline.toString()}</h1>
+              <h1>Days from now: {i.length("days")}</h1>
+              <h1>TODAY === {now.toString()}</h1>
+              <h1>Deadline: {deadline.toString()}</h1>
             </div>
           );
         })}
