47b0bbc203c852b2efa8377bceeff12db15fdc05
Jocelyn <115977531+tokziqijocelyn@users.noreply.github.com>
2024-01-11 22:20:15 +0800

    feat: implemented dayjs
    Locale string not found for day js

--- src/app/daysleft/page.tsx
+++ src/app/daysleft/page.tsx
@@ -3,43 +3,32 @@ import dayjs from "dayjs";
 import React, { useEffect, useState } from "react";
 import { api } from "@/trpc/react";
 import { DateTime, Interval } from "luxon";
-import timezone from "dayjs/plugin/timezone"; 
+import timezone from "dayjs/plugin/timezone";
+import relative from "dayjs/plugin/relativeTime";
+import locale from "dayjs/plugin/localeData";
 
 interface PageProps {}
 
 const Page: React.FC<PageProps> = () => {
+  //this is for user's timezone
   const [tzid, setTzid] = useState("");
-  const {
-    data: deadlineData,
-    isError,
-    isLoading,
-  } = api.date.getAllDates.useQuery();
+  const { data: deadlineData, isError, isLoading } = api.task.all.useQuery();
 
-  console.log("DEADLINE:", deadlineData);
   const now = new Date();
-  //getting the timezone from user's browser
-  //This will log client browser's timezone
-  console.log(tzid);
+
 
   type date = {
-    id: number;
+    id: string;
     name: string;
+    completed: boolean;
     createdAt: Date;
-    updatedAt: Date;
-    deadline: Date;
+    completeBy: Date;
   };
-<<<<<<< HEAD
 
   // timezone is set to my desired one (which is the client's timezone) only when it loads
   useEffect(() => {
     setTzid(Intl.DateTimeFormat().resolvedOptions().timeZone);
   }, []);
-=======
-// timezone is set to my desired one (which is the client's timezone) only when it loads
-useEffect(() => {
-  setTzid(Intl.DateTimeFormat().resolvedOptions().timeZone);
-}, [])
->>>>>>> 41f4cdf2ff9582cc34252c64eadebd7e5f35c7b8
   return (
     <div>
       <h1>Days left for each deadline</h1>
@@ -51,7 +40,7 @@ useEffect(() => {
         {!deadlineData && <div>No</div>}
         {deadlineData?.map((date: date) => {
           // so that I can use Luxon's date functions
-          let deadline = DateTime.fromISO(date.deadline.toISOString());
+          let deadline = DateTime.fromISO(date.completeBy.toISOString());
           //We need a comparison with the time now and the time in the database, both in local time
           const now = DateTime.local();
           const i = Interval.fromDateTimes(now, deadline);
@@ -64,13 +53,13 @@ useEffect(() => {
               <h1>Days from now: {Math.round(i.length("days"))} days</h1>
               <h1>TODAY === {now.toString()} in local time</h1>
               <h1>
-                Deadline: {deadline.toLocaleString(DateTime.DATETIME_FULL)} in
+                LOCALE {deadline.toLocaleString(DateTime.DATETIME_FULL)} in
                 local timezone in the respective locales:
               </h1>
               Converted from javascript datetime obj to luxon:
               {deadline.toString()}
               <h1>
-                Original Deadline in database {date.deadline.toISOString()} in
+                Original Deadline in database {date.completeBy.toISOString()} in
                 UTC
               </h1>
             </div>
@@ -78,29 +67,38 @@ useEffect(() => {
         })}
       </div>
       <div>
-        <h1 className="text-5xl">This is with Dayjs</h1>
+        <h1 className="text-5xl">
+          This is with Dayjs ___________________________
+        </h1>
         {!deadlineData && <div>No Dayjs</div>}
         {deadlineData?.map((date: date) => {
           // so that I can use Luxon's date functions
-          let deadline = dayjs(date.deadline.toISOString());
+          let deadline = dayjs(date.completeBy.toISOString());
           //We need a comparison with the time now and the time in the database, both in local time
+          // for dayjs, this is in UTC
           let now = dayjs();
+          dayjs.extend(timezone);
+          dayjs.extend(relative);
+          dayjs.extend(locale)
 
           return (
             <div className="p-2" key={date.id}>
               =============================
               <h1 className="p-3 text-4xl">{date.name}</h1>
-              <h1> DEADLINE TIMEZONE: {dayjs.tz.guess()}</h1>
-              <h1>Days from now: {now.diff(deadline)} days</h1>
+              <h1>
+                {" "}
+                DEADLINE TIMEZONE: {dayjs.tz.guess() || "This is undefined"}
+              </h1>
+              <h1>Days from now: {dayjs(now).to(deadline)}</h1>
               <h1>TODAY === {now.toString()} in local time</h1>
               <h1>
-                Deadline: {deadline.toLocaleString(DateTime.DATETIME_FULL)} in
+                LOCALE {} in
                 local timezone in the respective locales:
               </h1>
-              Converted from javascript datetime obj to luxon:
+              Converted from javascript datetime obj to dayjs:
               {deadline.toString()}
               <h1>
-                Original Deadline in database {date.deadline.toISOString()} in
+                Original Deadline in database {date.completeBy.toISOString()} in
                 UTC
               </h1>
             </div>
